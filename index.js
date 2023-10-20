0f7f801c367bf9b5e1d751c7c49e406c2fdfad7f
JFMartindale <jon4mart@gmail.com>
2023-10-20 14:58:36 -0500

    handle options styling

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 import { config } from "./config.js";
 
+
 const app = express();
 const port = 3000;
 
@@ -28,49 +29,61 @@ app.get("/", async (req, res) => {
 
 app.post("/", async (req, res) => {
 
-  console.log(req.body);
+//#region experimental array generator
+ 
+
+
+//const fs = require('fs');
+// var datastartdate=new Date("2000-01");
+// var dataenddate=new Date("")
+// console.log();
+// for(var n=0; n>=1;n++){
+
+// }
+
+
+// let data = "Learning how to write in a file."
+// fs.writeFile('Output.txt', data, (err) => { 
+//     if (err) throw err; 
+// });
+
+//#endregion 
+
+
+
+
+
+  //#region assign user submitted data
+  var dates=[];
+  var values=[];
+  var selecteddatapoints=[];
+  var selecteddates=[];
+  var selectedvalues=[];
+  var whatchart=req.body.subFrom;
+  console.log("success");
+  console.log(req.body.from);
   var fromDate=new Date(req.body.from+"+00:00");
   var toDate=new Date(req.body.to+"+00:00");
+  var symbol=req.body.textEntry;
+  symbol=symbol.toUpperCase();
+ 
+  console.log("success")
   console.log(fromDate);
-  console.log(toDate);
 
+  //#endregion 
 
-
-  
-  var timeSeries="TIME_SERIES_INTRADAY";
-  //var symbol=;
-  //var outputsize=;
-  //var interval=;
-  //var month=;
-
-  
-
-  if(timeSeries==="TIME_SERIES_INTRADAY"){
   const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${mykey}`);
   var responseData= response.data['Time Series (5min)'];//rids response of metadata and isolates information imidiately useful
 
-  //Generates Array for Intraday time and closing trade value
-  var dates=[];
-  var values=[];
-
-
-//console.log("dates after filter"+);
+ 
 
   for(const dateandtime in responseData){
     var closingValue=responseData[dateandtime]['4. close'];
     dates.push(dateandtime);
     values.push(Number(closingValue));
   }
- 
-//console.log("dates before filter"+dates);
-// var testdate=dates[0];
-// console.log(testdate);
-// console.log(typeof testdate);
-// var testdateasdate=new Date(testdate+"+00:00");
-// console.log(testdateasdate);
 
 
-var selecteddatapoints=[];
 
 dates.forEach(isolateTimeRange);
 function isolateTimeRange(ittDate,ittindex){
@@ -84,8 +97,7 @@ function isolateTimeRange(ittDate,ittindex){
 }
 console.log(selecteddatapoints);
 
-var selecteddates=[];
-var selectedvalues=[];
+
 
 selecteddatapoints.forEach(function (pointselected){
     selecteddates.push(dates[pointselected]);
@@ -93,10 +105,7 @@ selecteddatapoints.forEach(function (pointselected){
 });
 
   
-  }else if(timeSeries==="TIME_SERIES_DAILY"){}
-  else if(timeSeries==="TIME_SERIES_WEEKLY"){}
-  else if(timeSeries==="TIME_SERIES_MONTHLY"){}
-  else{}
+ 
     
     try {
      selecteddates=selecteddates.reverse();
