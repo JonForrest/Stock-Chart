915787f3e2120ecc726576a674237e7e52968705
JFMartindale <jon4mart@gmail.com>
2023-10-25 19:36:01 -0500

    make foldable organization in server code, write pseudocode for month calls in todo

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import bodyParser from "body-parser";
 import { config } from "./config.js";
 
-
 const app = express();
 const port = 3000;
 
@@ -12,111 +11,79 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var mykey = config.MY_KEY;
 
-
 app.get("/", async (req, res) => {
-
-
   try {
-      
     res.render("index.ejs", {});
   } catch (error) {
     console.error("Failed to make request:", error.message);
-
   }
-
 });
 
-
 app.post("/", async (req, res) => {
-
-//#region experimental array generator
- 
-
-
-//const fs = require('fs');
-// var datastartdate=new Date("2000-01");
-// var dataenddate=new Date("")
-// console.log();
-// for(var n=0; n>=1;n++){
-
-// }
-
-
-// let data = "Learning how to write in a file."
-// fs.writeFile('Output.txt', data, (err) => { 
-//     if (err) throw err; 
-// });
-
-//#endregion 
-
-
-
-
-
   //#region assign user submitted data
-  var dates=[];
-  var values=[];
-  var selecteddatapoints=[];
-  var selecteddates=[];
-  var selectedvalues=[];
-  var whatchart=req.body.subFrom;
-  console.log("success");
-  console.log(req.body.from);
-  var fromDate=new Date(req.body.from+"+00:00");
-  var toDate=new Date(req.body.to+"+00:00");
-  var symbol=req.body.textEntry;
-  symbol=symbol.toUpperCase();
- 
-  console.log("success")
+  var dates = [];
+  var values = [];
+  var selecteddatapoints = [];
+  var selecteddates = [];
+  var selectedvalues = [];
+  var whatchart = req.body.subFrom;
+  console.log("'From' from request body:" + req.body.from);
+  var fromDate = new Date(req.body.from + "+00:00");
+  var toDate = new Date(req.body.to + "+00:00");
+  var symbol = req.body.textEntry;
+  symbol = symbol.toUpperCase();
   console.log(fromDate);
 
-  //#endregion 
+  //#endregion
 
-  const response = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${mykey}`);
-  var responseData= response.data['Time Series (5min)'];//rids response of metadata and isolates information imidiately useful
+  //#region call API
+  const response = await axios.get(
+    `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=${mykey}`
+  );
+  var responseData = response.data["Time Series (5min)"]; //rids response of metadata and isolates information imidiately useful
+  //#endregion
 
- 
-
-  for(const dateandtime in responseData){
-    var closingValue=responseData[dateandtime]['4. close'];
+  //#region populate dates and values arrays
+  for (const dateandtime in responseData) {
+    var closingValue = responseData[dateandtime]["4. close"];
     dates.push(dateandtime);
     values.push(Number(closingValue));
   }
+  //#endregion
 
-
-
-dates.forEach(isolateTimeRange);
-function isolateTimeRange(ittDate,ittindex){
-  
-  const morphedDate=new Date(ittDate+"+0000");
-  if(morphedDate.getTime()>=fromDate.getTime()&&morphedDate.getTime()<=toDate){
-    selecteddatapoints.push(ittindex);
-    
+  //#region populate array full of index values for selected data points
+  dates.forEach(isolateTimeRange);
+  function isolateTimeRange(ittDate, ittindex) {
+    const morphedDate = new Date(ittDate + "+0000");
+    if (
+      morphedDate.getTime() >= fromDate.getTime() &&
+      morphedDate.getTime() <= toDate
+    ) {
+      selecteddatapoints.push(ittindex);
+    }
   }
+  //#endregion
 
-}
-console.log(selecteddatapoints);
-
-
-
-selecteddatapoints.forEach(function (pointselected){
+  //#region populate two new arrays (for dates and values) with only selected data points
+  selecteddatapoints.forEach(function (pointselected) {
     selecteddates.push(dates[pointselected]);
     selectedvalues.push(values[pointselected]);
+  });
+  //#endregion
+
+
+
+  try {
+    selecteddates = selecteddates.reverse();
+    selectedvalues = selectedvalues.reverse();
+    res.render("index.ejs", { dates: selecteddates, values: selectedvalues });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+  }
 });
 
-  
- 
-    
-    try {
-     selecteddates=selecteddates.reverse();
-     selectedvalues=selectedvalues.reverse();
-      res.render("index.ejs", {dates:selecteddates,values:selectedvalues});
-    } catch (error) {
-      console.error("Failed to make request:", error.message);
 
-    }
-  });
 
-  app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
